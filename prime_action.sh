2015a62d1e9fa7035a956c5070d008b0e3c753d2
Duddie <marcin.dudar@gmail.com>
2023-11-10 19:52:15 +0800

    script update

--- prime_action.sh
+++ prime_action.sh
@@ -3,5 +3,6 @@ printf "*** START *** \n\n"
 system_profiler SPHardwareDataType
 uname -a
 printf "\nProfiling...\n\n"
-for i in 1 2 3 4 5 6 ; do ./prime.elf ${i} ; done
+
+for i in {1..20} ; do ./prime.elf ${i} ; done
 printf "\n*** END ***\n"
